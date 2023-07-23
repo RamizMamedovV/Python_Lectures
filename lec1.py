457933a1fa27ada977a1bd56686730ef6ce63a68
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-07-23 09:18:30 +0300

    initial commit

---
+++ lec1.py
@@ -0,0 +1,18 @@
+# print("ctrl + f5")
+# print("<-")
+# print("python lec1.py")
+# print(5, 6, 5) # 5 6 5
+"""  закоментить кусок кода
+# # ctrl + k ; ctrl + c # закоментить
+# # ctrl + k ; ctrl + u # раскоментить
+"""
+
+# n = 5 # 5
+# n = None # None
+# n = 1.89 # 1.89
+# n = 'te\'xt' # te'xt
+# m = 'te"text"xt' # te"text"xt
+# print(n, m)
+# print(type(n)) # <class 'str'>
+
+
