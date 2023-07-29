f1194254a1114a51735844f7ccdbb27ad5a26194
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-07-29 05:13:04 +0300

    modified:   SEMINAR/task_example.py

--- SEMINAR/task_example.py
+++ SEMINAR/task_example.py
@@ -15,3 +15,39 @@ Output:
 #     print("Не корректный ввод")
 # # print(f"Потребуется дней: {(dist + per_day - 1) // per_day }") 
 # print(f"Потребуется дней: {math.ceil(dist / per_day)}")
+
+
+# x: int = 3
+# y: int = 5
+# print(x + y)          # 8
+
+# x: int = 'hello '
+# y: int = 'world'
+# print(x + y)            # hello world
+
+# x: int = 'hello '
+# y: int = 5
+# print(x * y)                # hello hello hello hello hello 
+
+# def sum(spisok):
+#     s = 0
+#     for i in spisok:
+#         s += i
+#     print(s)                    # без return
+
+# def sum1(spisok):
+#     s = 0
+#     for i in spisok:
+#         s += i
+#     return s                    # c return
+
+# def sum2(spisok: list) -> int:          # int - для аннотации результата!
+#     s: int = 0
+#     for i in spisok:
+#         s += i
+#     return s                    # c аннотацией для понимания! list,int
+
+
+
+# sum([5, 4, 3])
+# print(sum1([5, 4, 3]))
