f1ac2cfcfd33c4e81376cc971bda9ab0a209e3ce
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-08-10 19:59:09 +0300

    added task34

--- SEMINAR/Seminar6/def_str_list_to_num_list.py
+++ SEMINAR/Seminar6/def_str_list_to_num_list.py
@@ -1,7 +1,31 @@
-string = '-1*12*(40+(20 + 81 / 2))*(2+ 4)'
-i = 1
-sd = ''
-sd += string[i]
-lis = string.split()
+def count_vowels(word):
+    vowels = "AEIOUaeiou"  # Список гласных букв
+    count = 0
+    
+    for char in word:
+        if char in vowels:
+            count += 1
+            
+    return count
 
-print(sd)
+def main():
+    poem = input("Введите стихотворение: ")
+    lines = poem.split()  # Разделение стихотворения на строки
+    
+    prev_count = None  # Предыдущее количество слогов
+    
+    for line in lines:
+        words = line.split('-')  # Разделение строки на слова
+        
+        line_count = count_vowels(''.join(words))  # Подсчет гласных в строке
+        
+        if prev_count is None:
+            prev_count = line_count
+        elif prev_count != line_count:
+            print("Пам парам")
+            return
+    
+    print("Парам пам-пам")
+
+if __name__ == "__main__":
+    main()
